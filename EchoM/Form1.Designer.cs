289b64fbccdc4822b86b549a9bfa8a86d70f7728
Ndusft <156385337+aw4e@users.noreply.github.com>
2024-07-20 02:38:33 +0700

    Add Code

---
+++ EchoM/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace EchoM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            substatLabel = new Label();
+            substatDropdown = new ComboBox();
+            timeDropdown = new ComboBox();
+            timeLabel = new Label();
+            startButton = new Button();
+            stopButton = new Button();
+            groupBox1 = new GroupBox();
+            infoBox = new RichTextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // substatLabel
+            // 
+            substatLabel.AutoSize = true;
+            substatLabel.Location = new Point(12, 9);
+            substatLabel.Name = "substatLabel";
+            substatLabel.Size = new Size(112, 16);
+            substatLabel.TabIndex = 0;
+            substatLabel.Text = "Select Substat:";
+            // 
+            // substatDropdown
+            // 
+            substatDropdown.FormattingEnabled = true;
+            substatDropdown.Location = new Point(135, 6);
+            substatDropdown.Name = "substatDropdown";
+            substatDropdown.Size = new Size(121, 24);
+            substatDropdown.TabIndex = 1;
+            substatDropdown.Text = "Select One";
+            substatDropdown.SelectedValueChanged += substatDropdown_SelectedValueChanged;
+            // 
+            // timeDropdown
+            // 
+            timeDropdown.FormattingEnabled = true;
+            timeDropdown.Location = new Point(135, 36);
+            timeDropdown.Name = "timeDropdown";
+            timeDropdown.Size = new Size(121, 24);
+            timeDropdown.TabIndex = 3;
+            timeDropdown.Text = "Select One";
+            timeDropdown.SelectedValueChanged += timeDropdown_SelectedValueChanged;
+            // 
+            // timeLabel
+            // 
+            timeLabel.AutoSize = true;
+            timeLabel.Location = new Point(12, 39);
+            timeLabel.Name = "timeLabel";
+            timeLabel.Size = new Size(91, 16);
+            timeLabel.TabIndex = 2;
+            timeLabel.Text = "Select Time:";
+            // 
+            // startButton
+            // 
+            startButton.Location = new Point(12, 84);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(117, 23);
+            startButton.TabIndex = 4;
+            startButton.Text = "Start";
+            startButton.UseVisualStyleBackColor = true;
+            startButton.Click += startButton_Click;
+            // 
+            // stopButton
+            // 
+            stopButton.Location = new Point(135, 84);
+            stopButton.Name = "stopButton";
+            stopButton.Size = new Size(121, 23);
+            stopButton.TabIndex = 5;
+            stopButton.Text = "Stop";
+            stopButton.UseVisualStyleBackColor = true;
+            stopButton.Click += stopButton_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.Control;
+            groupBox1.Controls.Add(infoBox);
+            groupBox1.Location = new Point(12, 113);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(244, 196);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Information";
+            // 
+            // infoBox
+            // 
+            infoBox.BackColor = SystemColors.Control;
+            infoBox.BorderStyle = BorderStyle.None;
+            infoBox.Location = new Point(6, 22);
+            infoBox.Name = "infoBox";
+            infoBox.ReadOnly = true;
+            infoBox.Size = new Size(232, 168);
+            infoBox.TabIndex = 0;
+            infoBox.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(268, 321);
+            Controls.Add(groupBox1);
+            Controls.Add(stopButton);
+            Controls.Add(startButton);
+            Controls.Add(timeDropdown);
+            Controls.Add(timeLabel);
+            Controls.Add(substatDropdown);
+            Controls.Add(substatLabel);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "EchoM";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label substatLabel;
+        private ComboBox substatDropdown;
+        private ComboBox timeDropdown;
+        private Label timeLabel;
+        private Button startButton;
+        private Button stopButton;
+        private GroupBox groupBox1;
+        private RichTextBox infoBox;
+    }
+}
---
+++ EchoM/Form1.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace EchoM
+{
+    public partial class Form1 : Form
+    {
+        private ComboBoxItem selectedSubstatItem;
+        private ComboBoxItem selectedTimeItem;
+        private System.Threading.Timer autoClickTimer;
+        private int intValue;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, int dwExtraInfo);
+
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const uint MOUSEEVENTF_LEFTUP = 0x0004;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeComboBoxes();
+        }
+
+        private void InitializeComboBoxes()
+        {
+            substatDropdown.Items.Add(new ComboBoxItem("Crit Dmg", 1));
+            substatDropdown.Items.Add(new ComboBoxItem("Crit Rate", 2));
+
+            for (int i = 0; i <= 9; i++)
+            {
+                timeDropdown.Items.Add(new ComboBoxItem($"X{i}", i));
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (selectedSubstatItem == null || selectedTimeItem == null)
+            {
+                UpdateRichBox("Please select Substat and Time first.\n");
+                return;
+            }
+
+            if (selectedTimeItem.Value is int value)
+            {
+                intValue = value;
+                string text = GetStartMessage(intValue);
+                UpdateRichBox(text);
+                StartAutoClickTimer();
+            }
+        }
+
+        private string GetStartMessage(int value)
+        {
+            var messages = new Dictionary<int, string>
+            {
+                { 0, "00, '10', '20', '30', '40', '50'" },
+                { 1, "01, '11', '21', '31', '41', '51'" },
+                { 2, "02, '12', '22', '32', '42', '52'" },
+                { 3, "03, '13', '23', '33', '43', '53'" },
+                { 4, "04, '14', '24', '34', '44', '54'" },
+                { 5, "05, '15', '25', '35', '45', '55'" },
+                { 6, "06, '16', '26', '36', '46', '56'" },
+                { 7, "07, '17', '27', '37', '47', '57'" },
+                { 8, "08, '18', '28', '38', '48', '58'" },
+                { 9, "09, '19', '29', '39', '49', '59'" }
+            };
+
+            return messages.TryGetValue(value, out var availableTimes)
+                ? $"Starting Auto-Clicking with Minutes {value}\nAvailable times: '{availableTimes}'\nTime Zone: {TimeZoneInfo.Local.DisplayName}\n"
+                : "Value Tidak Tersedia.\n";
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            autoClickTimer?.Dispose();
+            autoClickTimer = null;
+            UpdateRichBox("Stopping Auto-Clicking\n");
+        }
+
+        private void substatDropdown_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (substatDropdown.SelectedItem != null)
+            {
+                selectedSubstatItem = (ComboBoxItem)substatDropdown.SelectedItem;
+                CheckAndUpdateRichBox();
+            }
+        }
+
+        private void timeDropdown_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (timeDropdown.SelectedItem != null)
+            {
+                selectedTimeItem = (ComboBoxItem)timeDropdown.SelectedItem;
+                CheckAndUpdateRichBox();
+            }
+        }
+
+        private void CheckAndUpdateRichBox()
+        {
+            if (selectedSubstatItem != null && selectedTimeItem != null)
+            {
+                string text = $"Substat: {selectedSubstatItem.Name}\nTime: {selectedTimeItem.Name}\n";
+                UpdateRichBox(text);
+            }
+        }
+
+        private void StartAutoClickTimer()
+        {
+            autoClickTimer?.Dispose();
+            autoClickTimer = new System.Threading.Timer(TimerCallback, null, 0, 10); // Check every 10 milliseconds
+        }
+
+        private void TimerCallback(object state)
+        {
+            DateTime now = DateTime.Now;
+            int lastTwoDigitsOfMilliseconds = (now.Millisecond / 10) % 100;
+
+            if (now.Minute % 10 == intValue &&
+                now.Second % 10 == intValue &&
+                lastTwoDigitsOfMilliseconds % 10 == intValue)
+            {
+                PerformClick();
+            }
+        }
+
+        private void PerformClick()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+
+        private void UpdateRichBox(string text)
+        {
+            if (infoBox.InvokeRequired)
+            {
+                infoBox.Invoke(new Action(() => infoBox.AppendText(text)));
+            }
+            else
+            {
+                infoBox.AppendText(text);
+            }
+        }
+
+        private class ComboBoxItem
+        {
+            public string Name { get; }
+            public object Value { get; }
+
+            public ComboBoxItem(string name, object value)
+            {
+                Name = name;
+                Value = value;
+            }
+
+            public override string ToString() => Name;
+        }
+    }
+}
